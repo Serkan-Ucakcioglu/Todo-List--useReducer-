37fd60ead369e29527a09938be16b386ef2383a8
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-15 15:27:34 +0300

    usereducer and context todo

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { createContext } from 'react';
+import { useReducer } from 'react';
 
 export const Context = createContext(null)
 
+const initialState = {
+    id: 0,
+    title: '',
+}
+
+const reducer = (state , {type,payload}) => {
+    switch (type) {
+        case 'ADD-TODO':
+            return [...state , addTodo(payload.title)]
+       
+    }
+}
+
+const addTodo = (name) => {
+    return {id: Date.now() , title: name}
+}
+
 const Provider = ({children}) => {
-    
+ const [state, dispatch] = useReducer(reducer, initialState)
+
     return(
       <Context.Provider>
         {children}
