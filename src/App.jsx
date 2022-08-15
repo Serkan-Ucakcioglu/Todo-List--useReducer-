0f2ded69949424c8ed63b1b5fd54e8bf07da7a49
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-15 16:49:33 +0300

    indent fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css';
-import TodoList from './Todo/TodoList';
-import TodoForm from './Todo/TodoForm';
+import "./App.css";
+import TodoList from "./Todo/TodoList";
+import TodoForm from "./Todo/TodoForm";
 
 function App() {
   return (
     <div className="App">
-     <div className="container">
-     <header>
-        <h1>Todo List</h1>
-      </header>
-      <TodoForm/>
-      <TodoList/>
-     </div>
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+        <TodoForm />
+        <TodoList />
+      </div>
     </div>
   );
 }
