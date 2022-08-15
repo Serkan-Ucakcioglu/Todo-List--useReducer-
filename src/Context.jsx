423805588b2fad87523fa432eeea7c000a04fe54
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-15 15:18:03 +0300

    context create

---
+++ src/Context.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createContext } from 'react';
+
+export const Context = createContext(null)
+
+const Provider = ({children}) => {
+    
+    return(
+      <Context.Provider>
+        {children}
+      </Context.Provider>
+    )
+}
+
+
+export default Context;
