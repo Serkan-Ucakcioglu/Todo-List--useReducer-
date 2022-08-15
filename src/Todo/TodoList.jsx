e2c9a24d3a84edec1e4eae1c175f05b183537252
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-15 16:38:46 +0300

    indent

--- src/Todo/TodoList.jsx
+++ src/Todo/TodoList.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../Context";
 
 const TodoList = () => {
   const { state, dispatch } = useContext(Context);
+
+  let [isEditing, setIsEditing] = useState(false);
 
   return (
     <ul className="todo-list">
       {state.map((todo) => (
         <li className="todo" key={todo.id}>
           <span>{todo.title}</span>
-          <button className="edit">edit</button>
+          <button
+            className="edit"
+            onClick={() => setIsEditing((isEditing = !isEditing))}
+          >
+            {isEditing ? "save" : "edit"}
+          </button>
           <div>
             <button
               className="delete"
